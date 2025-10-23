f0bce351f9b87f4b2e11309fcda622d02ae26068
wooryeol <86636999+wooryeol@users.noreply.github.com>
2025-10-23 13:14:53 +0900

    FEAT: 가게 목록 조회, 가게 상세 조회, 가게 등록 기능 추가

---
+++ storeservice/src/main/java/delivery_system/store/presentation/dto/request/ReqCreateStoreDtoV1.java
@@ -0,0 +1,38 @@
+package delivery_system.store.presentation.dto.request;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ReqCreateStoreDtoV1 {
+    @JsonProperty("store_id")
+    @NotBlank
+    private String storeId;
+
+    @JsonProperty("store_name")
+    @NotBlank(message = "가게명은 필수입니다.")
+    private String storeName; // 가게명
+
+    @JsonProperty("owner_id")
+    @NotBlank(message = "소유자 ID는 필수입니다.")
+    private String ownerId; //
+
+    @JsonProperty("description")
+    @NotBlank(message = "가게 소개는 필수입니다.")
+    private String description; // 가게 소개
+
+    @JsonProperty("store_address")
+    @NotBlank(message = "가게 주소는 필수입니다.")
+    private String storeAddress; // 주소 원문
+
+    @JsonProperty("store_rating_avg")
+    private Double storeRatingAvg; // 가게 평점
+
+    @JsonProperty("store_review_count")
+    private Integer storeReviewCount; // 리뷰 수
+}
--- storeservice/src/main/java/delivery_system/store/presentation/dto/response/ResGetStoresDtoV1.java
+++ storeservice/src/main/java/delivery_system/store/presentation/dto/response/ResGetStoresDtoV1.java
@@ -10,14 +10,14 @@ import java.util.UUID;
 @Getter
 @Setter
 @NoArgsConstructor
-public class ResGetStoreByIdDtoV1 {
+public class ResGetStoresDtoV1 {
     private UUID storeId; // 가게 식별자
     private String ownerId; // 점주 식별자
     private String storeName; // 가게명
     private String description; // 가게 소개
     private Integer deliveryFee; // 배달비
     private String storeAddress; // 주소 원문
-    private Point storeGeom; // 주소 좌표
+    private String storeGeom; // 주소 좌표
     private Double storeRatingAvg; // 평균 평점
     private Integer StoreReviewCount; // 리뷰 갯수
 }
